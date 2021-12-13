c47d414909bc95b84821f826bdc8ee6f54a7ec4a
hashrock <hashrock@users.noreply.github.com>
2021-12-14 00:54:42 +0900

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
+
+namespace hash_browser
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    void ButtonGo_Click(object sender, RoutedEventArgs e)
+    {
+      if (webView != null && webView.CoreWebView2 != null)
+      {
+        webView.CoreWebView2.Navigate(addressBar.Text);
+      }
+    }
+    private void OnKeyDownHandler(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Return)
+      {
+        ButtonGo_Click(sender, e);
+      }
+    }
+  }
+
+}
